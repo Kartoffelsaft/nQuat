763fd71124dadd7e8e6c688cad4c7f2473f45507
Ben Findley <ben.g.findley@gmail.com>
2020-07-25 22:07:44 -0700

    icosphere

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <ratio>
 #include <stdio.h>
 #include <vector>
@@ -7,19 +8,21 @@
 #include <ncurses.h>
 
 #include "quaternion/quaternion.h"
+#include "vectors/vector2.h"
 #include "vectors/vector3.h"
 #include "window/window.h"
 #include "rasterizer/rasterizer.h"
 
 int main()
 {
-    auto points = std::vector<Vector3>{
-        Vector3(-3, 2, 4),
-        Vector3(10, 2, 8),
-        Vector3(4, 2, -6)
+    auto points = std::vector<float>{
+        #include "../models/sphere.vtx"
     };
-    auto q = Quaternion(Vector3(1, 0, 0), 0.02);
-    auto qn = q.inverseSansNormalized();
+    auto const tris = std::vector<size_t>{
+        #include "../models/sphere.tri"
+    };
+    auto const q = Quaternion(Vector3(1, 1, 0), 0.01);
+    auto const qn = q.inverseSansNormalized();
 
     Window w;
 
@@ -28,25 +31,42 @@ int main()
         w.clean();
         auto [offsetX, offsetY] = w.middle();
 
-        auto const appearance = getAppearance(points[0], points[1], points[2]);
-
-        fillTriangle(
-            Vector2(points[0].x + offsetX, points[0].y + offsetY), 
-            Vector2(points[1].x + offsetX, points[1].y + offsetY), 
-            Vector2(points[2].x + offsetX, points[2].y + offsetY), 
-            w, appearance
-        );
-        
-        for(auto point: points)
+        for(auto i = 0; i < tris.size(); i += 3)
         {
-            w.drawChar(point.x + offsetX, point.y + offsetY, 'O');
-        }
+            auto const * const pa = &points[3 * (tris[i + 0] - 1)];
+            auto const * const pb = &points[3 * (tris[i + 1] - 1)];
+            auto const * const pc = &points[3 * (tris[i + 2] - 1)];
+            auto const va = Vector3(pa[0], pa[1], pa[2]);
+            auto const vb = Vector3(pb[0], pb[1], pb[2]);
+            auto const vc = Vector3(pc[0], pc[1], pc[2]);
 
+            auto const appearance = getAppearance(va, vb, vc);
+
+            auto const scale = 5;
+
+            fillTriangle(
+                Vector2(va.x * scale + offsetX, va.y * scale + offsetY), 
+                Vector2(vb.x * scale + offsetX, vb.y * scale + offsetY), 
+                Vector2(vc.x * scale + offsetX, vc.y * scale + offsetY), 
+                w, appearance
+            );
+        }
+        
         w.refreshScreen();
 
-        for(auto& point: points)
+        for(auto i = 0; i < points.size(); i += 3)
         {
+            auto point = Vector3(
+                points[i + 0],
+                points[i + 1],
+                points[i + 2]
+            );
+
             point = q * point * qn;
+            
+            points[i + 0] = point.x;
+            points[i + 1] = point.y;
+            points[i + 2] = point.z;
         }
 
         std::this_thread::sleep_for(std::chrono::milliseconds(16));
